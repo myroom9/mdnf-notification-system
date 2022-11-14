5a11292da34ddf39e8a360b7f072170470686e7f
wonhwiahn <myroom9@naver.com>
2022-11-14 14:19:36 +0900

    알람 옵션 조정 추가 및 og tag 추가

--- src/main/resources/database/init-data/alarm-send-option.sql
+++ src/main/resources/database/init-data/alarm-send-option.sql
@@ -1,5 +1,4 @@
 INSERT INTO alarm_send_options (alarm_type, send_flag) VALUES('MANUAL', 1);
 INSERT INTO alarm_send_options (alarm_type, send_flag) VALUES('NOTICE', 1);
-INSERT INTO alarm_send_options (alarm_type, send_flag) VALUES('DEV-NOTE', 1);
+INSERT INTO alarm_send_options (alarm_type, send_flag) VALUES('DEV_NOTE', 1);
 INSERT INTO alarm_send_options (alarm_type, send_flag) VALUES('UPDATE', 1);
-
